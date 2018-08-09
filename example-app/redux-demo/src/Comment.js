2b370eb1f2d02b846af6e603fa218f830b7bd6b2
karolinneczka <okaa93@gmail.com>
2018-08-09 15:20:53 +0200

    Add buttons and try to add dispatch in Comment

--- example-app/redux-demo/src/Comment.js
+++ example-app/redux-demo/src/Comment.js
@@ -1,7 +1,16 @@
 //wyświetlenie pojedynczego komentarza znajdującego się w komponencie z listą komentarzy
 
 import React from 'react';
+import { createStore } from 'redux';
+import reducer from './reducer.js';
+import {thumbUpComment} from './actions.js';
 
-const Comment = ({text, votes}) => <li>{text} <span>votes: {votes}</span></li>; //niech renderuja ise buttony i reaguja na klik --> store dispatch
+const store = createStore(reducer);
+const Comment = ({text, votes}) => 
+    <li>{text} 
+        <span>votes: {votes}</span>
+        <button className={'button'} id={'thumbUp'} onClick={(id) => store.dispatch(thumbUpComment(id))}>Thumb up</button>
+        <button className={'button'} id={'thumbDown'}>Thumb down</button>
+    </li>; //niech renderuja ise buttony i reaguja na klik --> store dispatch
 
 export default Comment;
